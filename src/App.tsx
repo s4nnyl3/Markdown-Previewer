4efdb38b4a4736427c258cc40de603d2e4f8cdb4
s4mmyl3 <sam.crafc@gmail.com>
2023-08-11 20:18:34 +0100

    Full Project

--- src/App.tsx
+++ src/App.tsx
@@ -41,7 +41,7 @@ function App() {
   return (
     <>
       <div>
-        <h1 style={{textAlign: "center"}}>Markdown Previewer</h1>
+        <h1 style={{textAlign: "center", color: "rgb(0, 255, 0)"}}>Markdown Previewer</h1>
         <div className="boxes-container">
           <textarea value={markdownText} onChange={(e) => setMarkdownText(e.target.value)} name="editor" id="editor"></textarea>
           <div id="preview">
